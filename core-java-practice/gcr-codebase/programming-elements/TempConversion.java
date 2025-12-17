7a5602e3348461702c94ee0ce847b9f9936d768d
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2025-12-17 15:38:55 +0530

    Added four question to gcr codebase

---
+++ core-java-practice/gcr-codebase/programming-elements/TempConversion.java
@@ -0,0 +1,9 @@
+
+import java.util.Scanner;
+public class TempConversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int temp = sc.nextInt();
+        System.out.println((temp*(9/5)+32));
+    }
+}
