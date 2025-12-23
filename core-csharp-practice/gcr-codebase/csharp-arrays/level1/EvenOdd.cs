da4e9a1e9c1cf5caa163dbacc5730f6f42162fcb
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2025-12-23 20:01:31 +0530

    Added arrays level 1 question to gcr-codebase/csharp-arrays/level1

---
+++ core-csharp-practice/gcr-codebase/csharp-arrays/level1/EvenOdd.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.arrays.level1
+{
+    internal class EvenOdd
+    {
+        public void FindEvenOdd()
+        {   //taking input from the user
+            int num = Convert.ToInt32(Console.ReadLine());
+
+
+            
+            int even = num/2;
+            int odd;
+
+            // detemining the size of the arrays
+            if (num % 2 == 0)
+            {
+                
+                odd = num / 2;
+            }
+            else 
+            {
+                odd = (num / 2) + 1;
+            }
+
+            // declaring and intilizing the array
+            int[] evenarr = new int[even];
+            int[] oddarr = new int[odd];
+            int j = 0;
+            int k = 0;
+
+            //putting even and odd numbers into theri respective arrays
+            for(int i = 1; i <= num; i++)
+            {
+                if (i % 2 == 0) 
+                { 
+                    evenarr[j] = i;
+                    j++;
+                }
+                else
+                {
+                    oddarr[k] = i;
+                    k++;
+                }
+
+            }
+
+            //printing the odd and even array one by one
+            Console.WriteLine("Printing even numbers");
+            for(int l = 0; l < even; l++)
+            {
+                Console.WriteLine(evenarr[l]);
+            }
+            Console.WriteLine("Printing odd numbers");
+            for(int m=0;m< odd; m++)
+            {
+                Console.WriteLine(oddarr[m]);
+            }
+        }
+    }
+}
---
+++ core-csharp-practice/gcr-codebase/csharp-arrays/level1/FizzBuzz.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.arrays.level1
+{
+    internal class FizzBuzz
+    {
+        public void FizzBuzzPrint()
+        {
+            //taking input from the user 
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            //calculating size and declaring array
+            //int three = num / 3;
+            //int five = num / 5;
+            //int both = num / 15;
+
+            //int size = three + five - both;
+
+            //string[] arr = new string[size];
+            //int count = 0;
+            //// checking divisibility
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    if (i % 3 == 0 && i % 5 == 0)
+            //    {
+            //        arr[count] = "FizzBuzz";
+            //        count++;
+            //    }
+            //    else if (i % 3 == 0)
+            //    {
+            //        arr[count] = "Fizz";
+            //        count++;
+            //    }
+            //    else if (i % 5 == 0)
+            //    {
+            //        arr[count] = "Buzz";
+            //        count++;
+            //    }
+            //    else
+            //    {
+            //        continue;
+            //    }
+            //}
+
+            //for (int j = 0; j < size; j++)
+            //{
+            //    Console.WriteLine(arr[j]);
+            //}
+
+            string[] arr = new string[num];
+
+            // checking divisibility
+            for (int i = 0; i < num; i++)
+            {
+                if ((i+1) % 3 == 0 && (i+1) % 5 == 0)
+                {
+                    arr[i] = "FizzBuzz";
+                }
+                else if ((i+1) % 3 == 0)
+                {
+                    arr[i] = "Fizz";
+                }
+                else if ((i+1) % 5 == 0)
+                {
+                    arr[i] = "Buzz";
+                }
+                else
+                {
+                    arr[i] = "Both not divisible";
+                }
+            }
+
+            for(int j = 0; j < num; j++)
+            {
+                Console.Write($"Number : {j+1} :");
+                Console.WriteLine(arr[j]);
+            }
+        }
+    }
+}
