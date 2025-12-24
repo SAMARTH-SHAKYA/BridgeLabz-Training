534ffbef99ee284a9b26dd5ac176f5d3935496a0
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2025-12-24 15:44:57 +0530

    Added Arrays level 2 programming question to gcr-codebase/arrays/level2

---
+++ core-csharp-practice/gcr-codebase/csharp-arrays/level2/ReverseNum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.arrays.level2
+{
+    internal class ReverseNum
+    {
+        public void Reverse()
+        {
+            //taking input from the user
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int len = Convert.ToString(num).Length;
+
+            int[] arr = new int[len];
+
+            int count = 0;
+
+            //applying logic to reverse
+
+            while (num > 0) {
+                arr[count] = num % 10;
+                num /= 10;
+                count++;
+            }
+
+            //printing the reverse number
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write(arr[i]);
+            }
+        } 
+    }
+}
