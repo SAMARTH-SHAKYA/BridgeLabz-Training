7353c5c9f0a07cac90f4257b1b96e9455265ee5b
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2025-12-25 15:02:27 +0530

    Added Methods Level 2 programming questions to gcr-codebase/csharp-methods/level2

---
+++ core-csharp-practice/gcr-codebase/csharp-methods/level2/LeapYear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.Methods.level2
+{
+    internal class LeapYear
+    {
+        public static void LeapOrNot(int year)
+        {
+            //applying logic to this code
+            if ((year % 4 == 0 && year % 100 != 0 && year>=1582) || (year % 400 == 0 && year>=1582))
+            {
+                Console.WriteLine("Leap Year");
+            }
+            else
+            {
+                Console.WriteLine("Not a Leap Year");
+            }
+        }
+        public static void main()
+        {
+            //taking input from the user
+            int year = Convert.ToInt32(Console.ReadLine());
+            LeapOrNot(year);
+        }
+    }
+}
---
+++ core-csharp-practice/gcr-codebase/csharp-methods/level2/SumNatural.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.Methods.level2
+{
+    internal class SumNatural
+    {
+        public static void main()
+
+        {
+            //taking input from the user
+            double n = Convert.ToInt32(Console.ReadLine());
+
+            if (n <= 0)
+            {
+                Console.WriteLine("Not valid");
+            }
+            //calling methods
+            double formula = Formula(n);
+
+            double recur = Recursive(n);
+
+            Console.WriteLine($"From formula the sum is {formula} and using the for loop the sum is {recur}");
+        }
+
+        public static double Formula(double n)
+        {
+            return (n * (n + 1)) / 2;
+        }
+
+        public static double Recursive(double n)
+        {
+            if (n == 0) {
+                return 0;
+            }
+            return n + Recursive(n - 1);
+        }
+    }
+}
