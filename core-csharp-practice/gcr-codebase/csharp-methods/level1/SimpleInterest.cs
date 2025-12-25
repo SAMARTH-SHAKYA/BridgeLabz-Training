d39d30fbd52b1a90bae2480a006c2a8553061414
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2025-12-25 12:16:48 +0530

    Added Methods Level 1 programming questions to gcr-codebase/csharp-methods/level1

---
+++ core-csharp-practice/gcr-codebase/csharp-methods/level1/SimpleInterest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.Methods.level1
+{
+    internal class SimpleInterest
+    {
+        //method to calculate interest 
+        public double CalInterest(double x, double y, double z)
+        {
+            return (x * y * z)/100.00; 
+        }
+
+
+        public void main()
+        {
+            //taking input from the user
+            double principle = Convert.ToDouble(Console.ReadLine());
+            double rate = Convert.ToDouble(Console.ReadLine());
+            double time = Convert.ToDouble(Console.ReadLine());
+
+            //calling method
+            double ans = CalInterest(principle, rate, time);
+
+            //printing result
+            Console.WriteLine($"The Simple Interest is {ans} for Principal {principle}, Rate of Interest {rate} and Time {time}");
+        }
+    }
+}
---
+++ core-csharp-practice/gcr-codebase/csharp-methods/level1/SmallLarge.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.Methods.level1
+{
+    internal class SmallLarge
+    {
+        //method to calcute the smallest and largest number
+        public void CalSmallLarge(int num1,int num2,int num3)
+        {
+            if(num1>num2 && num1 > num3)
+            {
+                Console.WriteLine($"{num1} largest");
+                if (num2 < num3)
+                {
+                    Console.WriteLine($"{num2} smallest");
+                }
+                else
+                {
+                    Console.WriteLine($"{num3} smallest");
+                }
+            }
+            else if(num2>num1 && num2 > num3)
+            {
+                Console.WriteLine($"{num2} largest");
+                if (num1 < num3)
+                {
+                    Console.WriteLine($"{num1} smallest");
+                }
+                else
+                {
+                    Console.WriteLine($"{num3} smallest");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{num3} largest");
+                if (num1 < num2)
+                {
+                    Console.WriteLine($"{num1} smallest");
+                }
+                else
+                {
+                    Console.WriteLine($"{num2} smallest");
+                }
+            }
+        }
+        public void main()
+        {
+            //taking input from the user
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            //calling the method
+            CalSmallLarge(num1,num2,num3);
+        }
+    }
+}
