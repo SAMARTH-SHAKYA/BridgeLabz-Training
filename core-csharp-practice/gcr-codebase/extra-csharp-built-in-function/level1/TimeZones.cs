bffa91defb417a8c902a9c51cd5b86b47413d3c3
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2025-12-28 21:24:37 +0530

    Refactor code

--- core-csharp-practice/gcr-codebase/extra-csharp-built-in-function/level1/TimeZones.cs
+++ core-csharp-practice/gcr-codebase/extra-csharp-built-in-function/level1/TimeZones.cs
@@ -6,5 +6,18 @@ namespace BridgeLabzTraining.builtIn.level1
 {
     internal class TimeZones
     {
+        public static void main()
+        {
+            DateTimeOffset utc = DateTimeOffset.UtcNow;
+            Console.WriteLine("UTC/GMT Time: " + utc);
+
+            TimeZoneInfo istZone = TimeZoneInfo.FindSystemTimeZoneById("India Standard Time");
+            DateTimeOffset ist = TimeZoneInfo.ConvertTime(utc, istZone);
+            Console.WriteLine("IST Time: " + ist);
+
+            TimeZoneInfo pstZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+            DateTimeOffset pst = TimeZoneInfo.ConvertTime(utc, pstZone);
+            Console.WriteLine("PST Time: " + pst);
+        }
     }
 }
