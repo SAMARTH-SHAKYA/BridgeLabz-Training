554f067710b99c8732220bcb8514f230369436f3
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2025-12-28 22:24:50 +0530

    Added extra built in functions programming questions level 2 to gcr-codebase/extra-built-in-function/level2

---
+++ core-csharp-practice/gcr-codebase/extra-csharp-built-in-function/level2/Prime.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.builtIn.level2
+{
+    internal class Prime
+    {
+        public static void main()
+        {
+            Console.WriteLine("Enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            CheckPrime(num);
+        }
+
+        public static void CheckPrime(int n)
+        {
+            if (n <= 1)
+            {
+                Console.WriteLine($"{n} is not a prime number");
+                return;
+            }
+
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine($"{n} is not a prime number");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{n} is a prime number");
+        }
+    }
+}
