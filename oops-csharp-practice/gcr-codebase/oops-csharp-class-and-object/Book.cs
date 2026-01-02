7f9bf62a292b92a1dd3005108747445e460412b5
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2026-01-02 15:44:18 +0530

    Updated the folder structure and added class and object question to gcr-codebase/oops-csharp-class-and-object

---
+++ oops-csharp-practice/gcr-codebase/oops-csharp-class-and-object/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.ClassAndObject
+{
+    class execute
+    {
+        public static void Main()
+        {
+
+
+            Book book = new Book("Day and Night", "Tony Stark", 3242);
+            //only read possible not write due to implemenation on read only
+            Console.WriteLine(book.Title);
+            Console.WriteLine(book.Author);
+            Console.WriteLine(book.Price);
+
+
+        }
+    }
+    
+
+    //making the book class
+    internal class Book
+        {
+            //applying only get for private fields
+            public string Author { get; }
+            public string Title { get; }
+            public double Price { get; }
+
+            //calling contructor
+            public Book(string title, string author, double price)
+            {
+                this.Title = title;
+                this.Author = author;
+                this.Price = price;
+            }
+        }
+
+}
+    
---
+++ oops-csharp-practice/gcr-codebase/oops-csharp-class-and-object/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.ClassAndObject
+{
+    //making class circle
+    internal class Circle
+    {
+        //intilizing field radius 
+        private double Radius;
+
+        //calling contructor
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+            Console.WriteLine($"Circle with radius {Radius}");
+        }
+
+        //method to calculate area
+        public double CalculateArea(double radius) 
+        {
+            double area =Math.PI*radius*radius;
+            return area;
+        }
+
+        //method to calcuate circumference
+        public double CalculateCircumference(double radius)
+        {
+            double circumference = Math.PI * radius * 2;
+            return circumference;
+        }
+
+    }
+}
