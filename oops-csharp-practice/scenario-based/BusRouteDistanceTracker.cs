bc2b20c05aa3303ca834cc9d820d5594efbe26ae
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2026-01-04 19:24:31 +0530

    Added cafeteria menu app, Festival lucky draw and Bus route distance tracker in the scenario based

---
+++ oops-csharp-practice/scenario-based/BusRouteDistanceTracker.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Collections.Generic;
+using System.Text;
+
+using System;
+
+namespace BridgeLabzTraining.Scenario_oops
+{
+    internal class BusRouteDistanceTracker
+    {
+        //method which will be called by the main
+        public void Run()
+        {
+            Console.WriteLine("-------------------- Bus Route Distance Tracker --------------------");
+
+            int totalDistance = 0;
+            bool isExit = false;
+
+            while (isExit == false)
+            {
+                //taking input from the user
+                Console.WriteLine("Enter distance for next stop (in km):");
+                int distance = Convert.ToInt32(Console.ReadLine());
+
+                if (distance <= 0)
+                {
+                    Console.WriteLine("Invalid distance");
+                    continue;
+                }
+
+                //calculating the total distance
+                totalDistance += distance;
+                Console.WriteLine($"Total Distance Travelled: {totalDistance} km");
+
+                Console.WriteLine("Do you want to get off at this stop? (yes/no)");
+                string choice = Console.ReadLine().ToLower();
+
+
+                //if choice yes the exit
+                if (choice == "yes")
+                {
+                    Console.WriteLine("Passenger got off the bus.");
+                    Console.WriteLine($"Final Distance Travelled: {totalDistance} km");
+                    isExit = true;
+                }
+            }
+        }
+    }
+}
+
---
+++ oops-csharp-practice/scenario-based/FestivalLuckyDraw.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System;
+
+namespace BridgeLabzTraining.Scenario_oops
+{
+    internal class FestivalLuckyDraw
+    {
+        //method which will be callled by main method
+        public void Run()
+        {
+            Console.WriteLine("-------------------- Diwali Lucky Draw --------------------");
+
+            bool isExit = false;
+
+            while (isExit == false)
+            {
+                //taking input from the user
+                Console.WriteLine("Enter your lucky number:");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+
+                //checking num is less than equal to 0
+                if (number <= 0)
+                {
+                    Console.WriteLine("Invalid number");
+                    continue;
+                }
+
+                //cheking the modulo condition
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("Congratulations! You won a gift 🎁");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, better luck next time");
+                }
+
+                Console.WriteLine("Is there another visitor? (yes/no)");
+                string choice = Console.ReadLine().ToLower();
+
+                if (choice == "no")
+                {
+                    isExit = true;
+                    Console.WriteLine("Lucky draw ended");
+                }
+            }
+        }
+    }
+}
+
