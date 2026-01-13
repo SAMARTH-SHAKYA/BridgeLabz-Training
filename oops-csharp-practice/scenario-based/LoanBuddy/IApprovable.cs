07cadbff7eed749bc3b78d00a4c6f2906eaa2dc7
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2026-01-13 15:51:12 +0530

    Added Loan buddy to scenario based folder

---
+++ oops-csharp-practice/scenario-based/LoanBuddy/IApprovable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.Scenario_oops.LoanBuddy
+{
+    internal interface IApprovable
+    {
+        public void ApproveLoan(Applicant applicant,LoanApplication application);
+
+        public void CalculateEMi(Applicant applicant, LoanApplication application);
+    }
+}
---
+++ oops-csharp-practice/scenario-based/LoanBuddy/LoanMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.Scenario_oops.LoanBuddy
+{
+    internal class LoanMain
+    {
+        public static void Main(string[] args)
+        {
+            LoanMenu menu = new LoanMenu();
+            menu.Menu();
+        }
+    }
+}
