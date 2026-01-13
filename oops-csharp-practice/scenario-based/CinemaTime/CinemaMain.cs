09062510e96fac97d080bd47bcebfb9320db9c4f
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2026-01-13 14:41:46 +0530

    Added cinema time in scenario based folder

---
+++ oops-csharp-practice/scenario-based/CinemaTime/CinemaMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.Scenario_oops.CinemaTime
+{
+    internal class CinemaMain
+    {
+        public static void Main(string[] args)
+        {
+            CinemaMenu menu = new CinemaMenu();
+            menu.Menu();
+        }
+    }
+}
