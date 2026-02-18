c4a7ae89addf46dc3d870e352654ae9a7868b150
SAMARTH SHAKYA <122541726+SAMARTH-SHAKYA@users.noreply.github.com>
2026-02-19 01:21:20 +0530

    Added concepts of collections to address book system

---
+++ collections-csharp-practice/scenario-based/AddressBookSystem/AddressBook.cs
@@ -0,0 +1,30 @@
+public class AddressBook
+{
+    //created address book class
+    public Contacts[] contacts {get ; private set;}
+
+    public string AddressBookName {get; private set;}   
+
+    public int AddressBookIdx {get; private set;}
+    public AddressBook(string name)
+    {
+        this.AddressBookName = name;
+        contacts = new Contacts[20];
+    } 
+
+
+    public void SetContacts(Contacts[] contact)
+    {
+        this.contacts = contact;
+    }
+
+    public void SetAddressBookName(string name)
+    {
+        this.AddressBookName = name;
+    }
+
+    public void SetAddressBookIdx(int idx)
+    {
+        this.AddressBookIdx = idx;
+    }
+}
